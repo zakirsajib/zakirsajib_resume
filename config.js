02f9ba34c82e8e3fed806b713fd23a9f2e70724d
Zakir Sajib <zakirsajib@gmail.com>
2020-02-01 10:49:19 +0000

    no message

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ module.exports = {
   description: 'I am a freelance developer with several years of experience working as a WordPress expert at Codeable and Upwork and has helped many clients around the world by building their online presence through high quality custom themes and plugins.',
   keywords: 'Visual Resume of Zakir Sajib, resume, developer, wordpress developer resume, cv',
   primaryColor: '#c0e3e7', // Required
-  googleAnalyticsID: 'UA-152596464-2',
-  googleSiteVerification: 'MGH2iJ6T5_R_hxmCHeikDehPlc7Ttn6eL_v-Owv0719',
+  googleAnalyticsID: 'UA-39208489-5',
+  googleSiteVerification: 'MGH2iJ6T5_R_hxmCHeikDehPlc7Ttn6eL_v-Owv0718',
   social: {
     website: 'https://nirvana.netlify.com',
     github: 'https://github.com/zakirsajib',
